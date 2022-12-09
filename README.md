1ef74ca380eec9868050621e1aa1c09f7265392b
lex <lex@users.noreply.github.com>
2022-12-09 19:51:38 +0200

    refactor, add support for hi-to-lo conversion

--- README.md
+++ README.md
@@ -1,30 +1,35 @@
 # snes-lohi-rom-convert
 
-snes-lohi-rom-convert is a simple command-line tool that allows you to convert SNES ROMs from LoROM format to HiROM format.
-
-Based on [LoHiROM](https://github.com/danielburgess/LoHiROM) by [danielburgess](https://github.com/danielburgess).
+This project provides a Python script that converts ROM images between the HiROM and LoROM formats used by the Super Nintendo Entertainment System (SNES).
 
 ## Background
 
-The Super Nintendo Entertainment System (SNES) uses two different memory addressing schemes for its ROMs: LoROM and HiROM. LoROM uses a 24-bit address space, which allows it to access up to 16 MB of ROM data. HiROM, on the other hand, uses a 32-bit address space, which allows it to access up to 4 GB of ROM data.
+The SNES is a video game console released by Nintendo in 1990. It uses a custom microprocessor, the Ricoh 5A22, which is based on the 16-bit WDC 65C816 CPU. The 5A22 has a 24-bit address bus, which allows it to address up to 16 MB of memory.
 
-LoROM and HiROM have different advantages and disadvantages. LoROM is generally considered to be faster than HiROM, but it has a limited address space, which can be a problem for larger games. HiROM, on the other hand, has a much larger address space, which allows it to accommodate larger games, but it is generally slower than LoROM.
+However, the SNES cartridge ROMs typically have a size of 1 MB (LoROM) or 2 MB (HiROM), and use a different addressing scheme than the 5A22 CPU. In LoROM, the first 64 KB of the cartridge ROM is mapped to the first 64 KB of the CPU address space, and the remaining 960 KB is mapped to the second 64 KB of the CPU address space, at addresses 0x8000-0xFFFF. In HiROM, the first 64 KB of the cartridge ROM is mapped to the second 64 KB of the CPU address space, at addresses 0x8000-0xFFFF, and the remaining 1.9 MB is mapped to the first 64 KB of the CPU address space.
+
+This addressing scheme allows the SNES to access the cartridge ROM in a way that is compatible with the 5A22 CPU, and enables the ROM to use the full address space of the CPU. However, it also means that the ROM must be converted between the LoROM and HiROM formats depending on the mapping used by the SNES.
 
 ## Usage
 
-To use snes-lohi-rom-convert, open a terminal and navigate to the directory where lohi.py is located. Then, run the following command:
+To use the `lohi.py` script, you need to have Python 3 installed on your computer. You can then run the script with the following command:
 
 ```sh
-python3 lohi.py [in_file_path] [out_file_path]
+$ python3 lohi.py [in_file_path] [out_file_path] [conversion_type]
 ```
 
-where `[in_file_path]` is the path to the input LoROM file and `[out_file_path]` is the path where the converted HiROM file should be saved.
+where `[in_file_path]` is the path to the input ROM image, `[out_file_path]` is the path to the output ROM image, and `[conversion_type]` is the type of conversion to perform. The `[conversion_type]` can be one of the following strings:
 
-For example, if the input LoROM file is located at `/home/user/roms/input.smc` and you want to save the converted HiROM file at `/home/user/roms/output.smc`, you would run the following command:
+- `lo-to-hi`: convert a LoROM image to a HiROM image.
+- `hi-to-lo`: convert a HiROM image to a LoROM image.
+
+For example, to convert a LoROM image at `"in.rom"` to a HiROM image and save it to `"out.rom"`, you can run the following command:
 
 ```sh
-python3 lohi.py /home/user/roms/input.smc /home/user/roms/output.smc
+$ python3 lohi.py in.rom out.rom lo-to-hi
 ```
+
+This command converts the LoROM image at `"in.rom"` to a HiROM image and saves it to `"out.rom"`.
 
 ## Requirements
 
--- lohi.py
+++ lohi.py
@@ -2,38 +2,85 @@ import sys
 
 
 def lo_hi_rom_convert(in_file_path, out_file_path):
-    try:
-        with open(in_file_path, "rb") as in_file:
-            l_file = in_file.read()
-        o_file = bytearray(len(l_file) * 2)
-        div = 0x8000
-        pcs = len(l_file) // div
-        hi_rom_pos = 0
-        pc_pos = 0
-        for c in range(pcs):
-            for d in range(div):
-                pc_pos = d + (c * div)
-                hi_rom_pos = d + (c * 0x10000)
-                o_file[hi_rom_pos] = l_file[pc_pos] if c != 0 else 0xFF
-                o_file[hi_rom_pos + div] = l_file[pc_pos]
-        with open(out_file_path, "wb") as out_file:
-            out_file.write(o_file)
-    except Exception as ex:
-        print_usage()
-        print(f"Error: {ex}")
+    # Open the input file in binary mode and read its content
+    with open(in_file_path, "rb") as in_file:
+        lo_rom = in_file.read()
+
+    # Check if the input ROM image is a valid LoROM image
+    if len(lo_rom) != 0x100000 or lo_rom[0x7FD5] != 0x20:
+        raise ValueError("Input ROM image is not a valid LoROM image")
+
+    # Create an empty HiROM image with twice the size of the input LoROM image
+    hi_rom = bytearray(len(lo_rom) * 2)
+
+    # Copy the first 64 KB of the LoROM image to the HiROM image
+    hi_rom[:0x10000] = lo_rom[:0x10000]
+
+    # Copy the remaining data from the LoROM image to the HiROM image,
+    # doubling each byte
+    for i in range(0x10000, len(lo_rom)):
+        hi_rom[i * 2] = lo_rom[i]
+        hi_rom[i * 2 + 1] = lo_rom[i]
+
+    # Set the ROM type in the HiROM image header to 0x21 (HiROM)
+    hi_rom[0x7FD5] = 0x21
+
+    # Open the output file in binary mode and write the content of the HiROM image
+    with open(out_file_path, "wb") as out_file:
+        out_file.write(hi_rom)
 
 
-def print_usage():
-    print("Usage: python3 lohi.py [in_file_path] [out_file_path]")
+def hi_lo_rom_convert(in_file_path, out_file_path):
+    # Open the input file in binary mode and read its content
+    with open(in_file_path, "rb") as in_file:
+        hi_rom = in_file.read()
+
+    # Check if the input ROM image is a valid HiROM image
+    if len(hi_rom) != 0x200000 or hi_rom[0x7FD5] != 0x21:
+        raise ValueError("Input ROM image is not a valid HiROM image")
+
+    # Create an empty LoROM image with half the size of the input HiROM image
+    lo_rom = bytearray(len(hi_rom) // 2)
+
+    # Copy the first 64 KB of the HiROM image to the LoROM image
+    lo_rom[:0x10000] = hi_rom[:0x10000]
+
+    # Copy the remaining data from the HiROM image to the LoROM image,
+    # averaging the data from each pair of bytes
+    for i in range(0x10000, len(lo_rom)):
+        lo_rom[i] = (hi_rom[i * 2] + hi_rom[i * 2 + 1]) // 2
+
+    # Set the ROM type in the LoROM image header to 0x20 (LoROM)
+    lo_rom[0x7FD5] = 0x20
+
+    # Open the output file in binary mode and write the content of the LoROM image
+    with open(out_file_path, "wb") as out_file:
+        out_file.write(lo_rom)
 
 
 def main():
-    if len(sys.argv) != 3:
-        print_usage()
-    else:
-        in_file_path = sys.argv[1]
-        out_file_path = sys.argv[2]
+    # Check if the correct number of command-line arguments were provided
+    if len(sys.argv) != 4:
+        print(
+            "Usage: python3 lohi.py [in_file_path] [out_file_path] [conversion_type]")
+        return
+
+    # Get the input and output file paths and the conversion type from the command-line arguments
+    in_file_path = sys.argv[1]
+    out_file_path = sys.argv[2]
+    conversion_type = sys.argv[3]
+
+    # Convert the input ROM image to a HiROM image if the conversion type is "lo-to-hi"
+    if conversion_type == "lo-to-hi":
         lo_hi_rom_convert(in_file_path, out_file_path)
+
+    # Convert the input ROM image to a LoROM image if the conversion type is "hi-to-lo"
+    elif conversion_type == "hi-to-lo":
+        hi_lo_rom_convert(in_file_path, out_file_path)
+
+    # Print an error message if the conversion type is not "lo-to-hi" or "hi-to-lo"
+    else:
+        print("Error: Invalid conversion type. Must be 'lo-to-hi' or 'hi-to-lo'.")
 
 
 if __name__ == "__main__":
